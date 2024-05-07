892162aa33b2fa4529181ce0a36e3ca118275c07
nisketch <nimrodogoro@gmail.com>
2024-05-07 17:22:57 +0300

    add map class

--- geopiy/geopiy.py
+++ geopiy/geopiy.py
@@ -2,6 +2,6 @@
 import ipyleaflet
 
 class Map(ipyleaflet.Map):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, center=[-25, 35], zoom= 5, **kwargs):
+        super().__init__(center=center, zoom=zoom, **kwargs)
         self.add_control(ipyleaflet.LayersControl())
